3e2ae70016d6eeecf0ac6dae1337d4637612a346
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2021-02-11 20:29:07 +0900

    added after format

--- scripts/git/hooks/pre-commit/rustfmt.sh
+++ scripts/git/hooks/pre-commit/rustfmt.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+set -eu
+wd=$(dirname $0)/../../..
+cd $wd
 file_path=$1
+
 rustfmt $file_path
+$wd/scripts/git/hooks/utils/after_format.sh $file_path
---
+++ scripts/git/hooks/utils/after_format.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -eu
+file_path=$1
+
+git add $file_path
+if "$(git diff --name-only --cached)" == ""; then
+	exit 1
+fi
