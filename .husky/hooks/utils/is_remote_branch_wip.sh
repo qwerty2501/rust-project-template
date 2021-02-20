287ab37f6f3511127c986f14fc7074ec428753a7
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2021-02-21 01:16:16 +0900

    fixed branch_name check

--- .husky/hooks/utils/is_remote_branch_wip.sh
+++ .husky/hooks/utils/is_remote_branch_wip.sh
@@ -2,10 +2,10 @@
 
 set -eu
 
-echo "REMOTE_REF:$GIT_HOOKS_REMOTE_REF"
-case "${GIT_HOOKS_REMOTE_REF##refs/heads/}"
-	wip/*) exit 0;;
-esac
+branch_name="${GIT_HOOKS_REMOTE_REF##refs/heads/}"
+if [[ "$branch_name" == "wip/"* ]]; then
+	exit 0
+fi
 
 exit 1
 
