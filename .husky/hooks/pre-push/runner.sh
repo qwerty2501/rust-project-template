7c76ea5271a74ceb6564a82c24d478f336ec76b4
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2021-02-21 00:00:25 +0900

    changed to environement variables.

--- .husky/hooks/pre-push/runner.sh
+++ .husky/hooks/pre-push/runner.sh
@@ -5,20 +5,12 @@ set -eu
 cr=$(dirname $0)
 cd $cr
 
-remote="$1"
-url="$2"
-local_ref="$3"
-local_sha="$4" 
-remote_ref="$5" 
-remote_sha="$6"
-range="$7"
-
 
 procs=()
 for entry in ./*
 do
 	if [ "$(basename $0)" != "$(basename $entry)" ]; then
-		$entry $remote $url $local_ref $local_sha $remote_ref $remote_sha $range &
+		$entry &
 		procs=("${procs[@]}" $!)
 	fi
 done
